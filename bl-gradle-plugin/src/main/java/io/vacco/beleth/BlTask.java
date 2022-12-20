9e9cdedc08b0900f5e16c84475e0ac45dc7263cd
Jesus Zazueta <jjzazuet@vacco.io>
2022-12-20 04:11:23 -0500

    - Gradle plugin fixes.

--- bl-gradle-plugin/src/main/java/io/vacco/beleth/BlTask.java
+++ bl-gradle-plugin/src/main/java/io/vacco/beleth/BlTask.java
@@ -5,11 +5,9 @@ import org.gradle.api.DefaultTask;
 import org.gradle.api.plugins.JavaPluginExtension;
 import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.*;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.*;
 import java.util.*;
 
 public abstract class BlTask extends DefaultTask {
@@ -20,7 +18,20 @@ public abstract class BlTask extends DefaultTask {
     var prj = getProject();
     var jxt = prj.getExtensions().getByType(JavaPluginExtension.class);
     var ssMain = jxt.getSourceSets().findByName("main");
-    ssMain.getJava().srcDir(getTargetHelmSourcesDir());
+    var helmSrc = getTargetHelmSourcesDir();
+    ssMain.getJava().srcDir(helmSrc);
+    getOutputs().dir(helmSrc);
+  }
+
+  private void delete(File dir) throws IOException {
+    if (dir.exists()) {
+      var fs = Files.walk(dir.toPath());
+      try (fs) {
+        fs.sorted(Comparator.reverseOrder())
+          .map(Path::toFile)
+          .forEach(File::delete);
+      }
+    }
   }
 
   private File getBuildDir() {
@@ -39,6 +50,7 @@ public abstract class BlTask extends DefaultTask {
 
   @TaskAction public void action() throws IOException {
     var helmSrc = getTargetHelmSourcesDir();
+    delete(helmSrc);
     helmSrc.mkdirs();
     for (URI u : getHelmPackages().get()) {
       var javaSrc = new BlHelmGen().apply(u.toURL(), getTargetBuildDir(), new BlLogger());

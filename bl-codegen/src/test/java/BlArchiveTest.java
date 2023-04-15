fb514de1cc0f4ee912a3693b3db5421445bf0e8e
jjzazuet <jjzazuet@vacco.io>
2023-04-15 17:12:11 -0400

    - WIP.

--- bl-codegen/src/test/java/BlArchiveTest.java
+++ bl-codegen/src/test/java/BlArchiveTest.java
@@ -36,9 +36,12 @@ public class BlArchiveTest {
       helmJavaSrc.mkdirs();
 
       var chartIdx = obj(
+        kv("https://charts.bitnami.com/bitnami/clickhouse-3.1.6.tgz", "com.bitnami.clickhouse"),
+        kv("https://charts.bitnami.com/bitnami/apache-9.2.2.tgz", "com.bitnami.apache"),
+        kv("https://argoproj.github.io/argo-helm/argo-1.0.0.tgz", "com.github.argo"),
         kv("https://scylla-operator-charts.storage.googleapis.com/stable/scylla-v1.8.0.tgz", "com.scylladb"),
         kv("https://charts.cockroachdb.com/cockroachdb-10.0.6.tgz", "com.cockroachdb"),
-        kv("https://github.com/metallb/metallb/releases/download/metallb-chart-0.13.9/metallb-0.13.9.tgz", "io.metallb"),
+        kv("https://github.com/metallb/metallb/releases/download/metallb-chart-0.13.9/metallb-0.13.9.tgz", "metallb.io"),
         kv("https://github.com/jenkinsci/helm-charts/releases/download/jenkins-4.3.20/jenkins-4.3.20.tgz", "com.github.jenkinsci"),
         kv("https://github.com/fluxcd-community/helm-charts/releases/download/flux2-2.6.0/flux2-2.6.0.tgz", "com.github.fluxcd"),
         kv("https://github.com/prometheus-community/helm-charts/releases/download/kube-prometheus-stack-45.10.0/kube-prometheus-stack-45.10.0.tgz", "com.github.prometheus"),

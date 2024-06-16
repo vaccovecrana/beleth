32d486c6478c99a5a3a334d5f380af65afaf6a82
Jesus Zazueta <jjzazuet@gmail.com>
2024-06-15 21:57:50 -0400

    - Resource management fixes.

--- bl-k8s-rt/src/main/java/io/vacco/beleth/rt/BlKubeRt.java
+++ bl-k8s-rt/src/main/java/io/vacco/beleth/rt/BlKubeRt.java
@@ -2,6 +2,7 @@ package io.vacco.beleth.rt;
 
 import java.util.*;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class BlKubeRt {
@@ -14,20 +15,48 @@ public class BlKubeRt {
     return this;
   }
 
-  public void commit(String packageName) {
-    var txIdx = manifests.stream()
-      .map(obj -> ctl.sync(obj, packageName))
-      .collect(Collectors.toMap(tx -> tx.blId, Function.identity()));
-    var clRes = ctl.resourceIndex(true);
-    clRes.putAll(ctl.resourceIndex(false));
-    clRes.keySet().removeAll(txIdx.keySet());
-    for (var e : clRes.entrySet()) {
+  private void delete(Map<String, List<BlKubeRes>> resourceIdx, String packageName, Predicate<BlKubeRes> condition) {
+    for (var e : resourceIdx.entrySet()) {
       for (var res : e.getValue()) {
-        if (res.blId.startsWith(packageName)) {
+        if (res.blId.startsWith(packageName) && condition.test(res)) {
           ctl.delete(res);
         }
       }
     }
+  }
+
+  private void delete(Map<String, List<BlKubeRes>> resourceIdx, String packageName) {
+    delete(resourceIdx, packageName, res ->
+      !(res.type.equals("persistentvolumeclaims")
+        || res.type.equals("persistentvolumes")
+        || res.type.equals("namespaces")
+      )
+    );
+    delete(resourceIdx, packageName, res ->
+      !(res.type.equals("persistentvolumes") || res.type.equals("namespaces"))
+    );
+    delete(resourceIdx, packageName, res ->
+      !(res.type.equals("namespaces"))
+    );
+    delete(resourceIdx, packageName, res -> // pesky bee...
+      res.type.equals("namespaces")
+    );
+  }
+
+  public void commit(String packageName) {
+    var txIdx = manifests.stream()
+      .map(obj -> ctl.sync(obj, packageName))
+      .collect(Collectors.toMap(tx -> tx.blId, Function.identity()));
+    var resIdx = ctl.resourceIndex(true);
+    resIdx.putAll(ctl.resourceIndex(false));
+    resIdx.keySet().removeAll(txIdx.keySet());
+    delete(resIdx, packageName);
+  }
+
+  public void deleteAll(String packageName) {
+    var resIdx = ctl.resourceIndex(true);
+    resIdx.putAll(ctl.resourceIndex(false));
+    delete(resIdx, packageName);
   }
 
 }

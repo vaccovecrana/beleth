67958ea3f33477bcc2b97d7d0b747d00cab5354a
Jesus Zazueta <jjzazuet@gmail.com>
2024-07-13 01:50:25 -0400

    - Patch version bump.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins { id("io.vacco.oss.gitflow") version "1.0.1" apply(false) }
 subprojects {
   apply(plugin = "io.vacco.oss.gitflow")
   group = "io.vacco.beleth"
-  version = "0.2.3"
+  version = "0.2.4"
 
   configure<io.vacco.oss.gitflow.GsPluginProfileExtension> {
     sharedLibrary(true, false)
